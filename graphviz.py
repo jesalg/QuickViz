5554a1c85f2a51fa8e368f333e40a3aea6a46a66
Jesal Gadhia <jesal7@gmail.com>
2020-06-14 16:45:52 -0700

    Updated graphviz filter

--- graphviz.py
+++ graphviz.py
@@ -11,35 +11,16 @@ import sys
 
 import pygraphviz
 
-from pandocfilters import toJSONFilter, Para, Image, get_filename4code
+from pandocfilters import toJSONFilter, Para, RawBlock, Image, get_filename4code
 from pandocfilters import get_caption, get_extension, get_value
-
-def document_name():
-    if "PANDOC_INPUT_FILE" in os.environ:
-        return os.environ["PANDOC_INPUT_FILE"]
-    else:
-        return "graph"
 
 def graphviz(key, value, format, _):
     if key == 'CodeBlock':
         [[ident, classes, keyvals], code] = value
         if "graph" in classes:
-            caption, typef, keyvals = get_caption(keyvals)
-            prog, keyvals = get_value(keyvals, u"prog", u"dot")
-            filetype = get_extension(format, "svg", html="svg", latex="pdf")
-            dest = get_filename4code(document_name(), code, filetype)
-
-            if not os.path.isfile(dest):
-                g = pygraphviz.AGraph(string=code)
-                g.layout()
-                g.draw(dest, prog=prog)
-                sys.stderr.write('Created image ' + dest + '\n')
-
-            image = Image([ident, classes, keyvals], 
-                          caption, 
-                          [dest, typef])
-
-            return Para([image])
+            g = pygraphviz.AGraph(string=code)
+            g.layout()
+            return RawBlock('html', g.draw(None, 'svg'))
 
 if __name__ == "__main__":
 
