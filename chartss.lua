7b4b91ebcaf7c4395d102eec7b9207ee35d3a4df
Jesal Gadhia <jesal7@gmail.com>
2020-06-14 16:45:34 -0700

    Adding mathjax

--- chartss.lua
+++ chartss.lua
@@ -88,6 +88,8 @@ function Doc(body, metadata, variables)
   add('<link rel="stylesheet" href="https://rbitr.github.io/ChartS.css/ChartSS.css" />')
   -- a default good style
   add('<link rel="stylesheet" href="https://unpkg.com/mvp.css" />')
+  add('<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>')
+  add('<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>')
   add('</head>')
   add('<body style="margin:0 3%">')
   add(body)
